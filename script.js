65591b5e960292d748797e7594c34aa7ae47b133
Glitch (hello-webpage) <none>
2019-04-19 23:13:09 +0000

    💺💐 Checkpoint ./script.js:378534/181

--- script.js
+++ script.js
@@ -6,23 +6,24 @@ canvas.height = ctx.height = window.innerHeight
 
 ctx.lineWidth = 1
 
-const panX = 2
+const panX = 1.5
 const panY = 1.5
-const mag = 500
+const mag = 100
 
 function checkSet(z, i) {
   let real = z
   let imaginary = i
   
   for (let j = 0; j < 100; j++) {
-    let tmpZ = real * real - imaginary * imaginary + z
-    let tmpI = 2 * real * imaginary + i
+    let tmpZ = (real * real) - (imaginary * imaginary) + z
+    let tmpI = (2 * real * imaginary) + i
     real = tmpZ
     imaginary = tmpI
   }
   
   if (real * imaginary < 5) {
-    ctx.fillRect(z * 200, i * 200, 1, 1)
+   // console.log(z * 100, i * 100)
+    ctx.fillRect(z * 100 + xh, i * 100, 1, 1)
   }
 }
 
