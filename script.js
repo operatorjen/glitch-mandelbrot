6e256641664b0496fb83ae15108d9356ab2014eb
Glitch (hello-webpage) <none>
2019-04-19 22:51:08 +0000

    🎄⚓️ Checkpoint ./script.js:378534/942

--- script.js
+++ script.js
@@ -7,27 +7,31 @@ canvas.height = ctx.height = 200
 ctx.lineWidth = 1
 
 const C = 0.1
-let z = Math.random()
-let i = Math.random()
-let panX = 1.5
-let panY = 1.5
-const mag = 200
+let panX = 0
+let panY = 0
+const mag = 600
 
-function draw() {
-  z = z * z - i * i + z
-  i = 2 * z * i + i
+function checkSet(z, i) {
+  let real = z
+  let imaginary = i
   
-  if (z * i < 5) {
-    ctx.fillRect(z / mag - panX, i / mag - panY, 1, 1)
-  } else {
-    z = Math.random()
-    i = Math.random()
+  for (let j = 0; j < 10; j++) {
+    z = z * z - i * i + z
+    i = 2 * z * i + i
+    real = z
+    imaginary = i
+  }
+  
+  if (real * imaginary < 5) {
+    ctx.fillRect(z, i, 1, 1)
   }
 }
 
 function render() {
-  for (let x = 0; x < 10; x++) {
-    draw()
+  for (let x = 0; x < canvas.width; x++) {
+    for (let y = 0; y < canvas.height; y++) {
+      checkSet(x / mag - panX, y / mag - panY)
+    }
   }
 }
 
