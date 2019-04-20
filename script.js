2869aae7a96920d9769e15fcfe6e04e6281699b2
Glitch (hello-webpage) <none>
2019-04-20 20:38:35 +0000

    👩‍👩‍👦🌑 Checkpoint ./script.js:378534/659

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
-const maxVal = document.querySelector('#max')
 
-let max = 100
+let max = 1
 
 canvas.width = ctx.width = window.innerWidth
 canvas.height = ctx.height = window.innerHeight
@@ -11,7 +10,7 @@ ctx.lineWidth = 1
 
 const panX = 0
 const panY = 0
-const mag = 1000
+const mag = 500
 let counter = 1
 
 function checkSet(z, i) {
@@ -28,28 +27,30 @@ function checkSet(z, i) {
   const total = real * imaginary * 100
   
   if (total < max * 2) {
-    //maxVal.value = total
-    if (total < -30) {
+    if (total < -1500) {
       ctx.fillStyle = 'rgb(1, 1, 1)'
-    } else if (total < -10) {
-      ctx.fillStyle = 'rgb(180, 50, 220)'
+    } else if (total < -150) {
+      ctx.fillStyle = 'rgb(20, 200, 20)'
+    } else if (total < -70) {
+      ctx.fillStyle = 'rgb(1, 1, 1)'
+    } else if (total < -30) {
+      ctx.fillStyle = 'rgb(80, 150, 220)'
     } else if (total < -5) {
       ctx.fillStyle = 'rgb(20, 140, 230)'
-    } else if (total < 0) {
-      ctx.fillStyle = 'rgb(225, 30, 200)'  
-    } else if (total < 15) {
-      ctx.fillStyle = 'rgb(205, 20, 220)'
-    } else if (total < 30) {
-      ctx.fillStyle = 'rgb(180, 20, 200)' 
+    } else if (total < 10) {
+      ctx.fillStyle = 'rgb(25, 130, 220)'  
+    } else if (total < 35) {
+      ctx.fillStyle = 'rgb(20, 220, 220)'
+    } else if (total < 60) {
+      ctx.fillStyle = 'rgb(220, 20, 100)' 
     } else {
       ctx.fillStyle = 'rgb(10, 180, 220)'  
     }
-    ctx.fillRect(z * 1000, i * 1000, 2, 2)
+    ctx.fillRect(z * 500, i * 500, 1, 1)
   }
 }
 
 function render() {
-  //ctx.clearRect(0, 0, canvas.width, canvas.height)
   for (let x = 0; x < canvas.width; x++) {
     for (let y = 0; y < canvas.height; y++) {
       checkSet(x / mag - panX, y / mag - panY)
@@ -65,7 +66,7 @@ function loop() {
   } else {
     max ++
   }
-  if (max > 100 || max < 1) {
+  if (max > 200 || max < 1) {
     switchs = !switchs 
   }
   
