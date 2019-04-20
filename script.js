fd185e3852bd97568173505a0ba7a8b607889400
Glitch (hello-webpage) <none>
2019-04-20 16:18:05 +0000

    🎉💂 Checkpoint ./script.js:378534/529

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ const maxVal = document.querySelector('#max')
 
 let max = 1
 
-maxVal.onchange = function () {
+maxVal.onblur = maxVal.onchange = function () {
   max = parseInt(this.value, 10)
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
   render()
 }
 
@@ -14,16 +15,16 @@ canvas.height = ctx.height = window.innerHeight
 
 ctx.lineWidth = 1
 
-const panX = 2.2
-const panY = 1.2
-const mag = 300
+const panX = 0
+const panY = 0
+const mag = 1000
 let counter = 1
 
 function checkSet(z, i) {
   let real = z
   let imaginary = i
   
-  for (let j = 0; j < 10; j++) {
+  for (let j = 0; j < max; j++) {
     let tmpZ = (real * real) - (imaginary * imaginary) + z
     let tmpI = (2 * real * imaginary) + i
     real = tmpZ
@@ -32,16 +33,34 @@ function checkSet(z, i) {
   
   if (real * imaginary < max) {
    // console.log(z * 100, i * 100)
-    ctx.fillRect(z * 1000, i * 1000, 2, 2)
+    ctx.fillRect(z * 500, i * 500, 1, 1)
   }
 }
 
 function render() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
   for (let x = 0; x < canvas.width; x++) {
     for (let y = 0; y < canvas.height; y++) {
-      checkSet(x / mag - panX, y / mag - panY)
+      checkSet(x / mag + panX, y / mag - panY)
     }
   }
 }
 
-render()
+let switchs = false
+
+function loop() {
+  if (!switchs) {
+    max++
+  } else {
+    max--  
+  }
+  if (max > 200 || max < 2) {
+    switchs = !switchs 
+  }
+  
+  render()
+  
+  requestAnimationFrame(loop, 500)
+}
+
+loop()
